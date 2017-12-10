09bb954cd07378d9009c5c739853bac4a4cc3030
Test Gulshan <test.gulshan@gmail.com>
2017-12-10 13:00:06 +0530

    D3.js implementation.

--- src/components/animation/Circle.js
+++ src/components/animation/Circle.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Circle = (props) => {
   const {value} = props;
   return (
     <span style={{width: value.x, height: value.x}} ></span>
   );
-}
+};
+
+
+Circle.propTypes = {
+  value: PropTypes.object.isRequired
+};
+
 
 export default Circle;
