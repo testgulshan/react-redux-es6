375a8191b75d7949b81fdc72fb117a8af4572e91
Tapas Coaching <tapascoaching@gmail.com>
2017-12-17 02:51:11 +0530

    Implemented Thunk middleware for Async requests with the help of Mock API.

--- src/components/pages/CoursesPage.js
+++ src/components/pages/CoursesPage.js
@@ -3,58 +3,27 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as courseActions from '../../actions/courseActions';
+import CourseList from './CourseList';
 
 class CoursesPage extends React.Component {
   constructor(props, context) {
     super(props, context);
-
-    this.state = {
-      course: { title: '' }
-    };
-
-    this.onTitleChange = this.onTitleChange.bind(this);
-    this.onClickSave = this.onClickSave.bind(this);
   }
-
-  onTitleChange(event) {
-    const course = this.state.course;
-    course.title = event.target.value;
-    this.setState({ course });
-  }
-
-  onClickSave() {
-    this.props.actions.createCourse(this.state.course);
-    this.setState({ course: { title: '' } });
-  }
-
+  
   courseRow(course, index) {
     return <li key={index}>{course.title}</li>;
   }
 
   render() {
+    const {courses} = this.props;
     return (
-      <div>
-        <h1>Courses</h1>
-        <h2>Add Course</h2>
-        <ol>
-          {this.props.courses.map(this.courseRow)}
-        </ol>
-        <input
-          type="text"
-          onChange={this.onTitleChange}
-          value={this.state.course.title} />
-        <input
-          type="submit"
-          value="Save"
-          onClick={this.onClickSave} />
-      </div>
+      <CourseList courses={courses}/>
     );
   }
 }
 
 CoursesPage.propTypes = {
-  courses: PropTypes.array.isRequired,
-  actions: PropTypes.object.isRequired
+  courses: PropTypes.array.isRequired
 };
 
 function mapStateToProps(state, ownProps) {
@@ -63,10 +32,4 @@ function mapStateToProps(state, ownProps) {
   };
 }
 
-function mapDispatchToProps(dispatch) {
-  return {
-    actions: bindActionCreators(courseActions, dispatch)
-  };
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
+export default connect(mapStateToProps)(CoursesPage);
